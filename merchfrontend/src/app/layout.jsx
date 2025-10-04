f7d39c39e2e3259cefe68a02475de1bb41d90ba8
NSuryansh <nagar.suryansh@gmail.com>
2025-10-04 16:15:08 +0530

    cahnges

--- merchfrontend/src/app/layout.jsx
+++ merchfrontend/src/app/layout.jsx
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.className}>
       <body className="bg-white text-gray-900">{children}</body>
     </html>
   );
