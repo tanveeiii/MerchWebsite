5d01d56af8ddb99bb08a8005854ad8bd44b8b923
Kartikey <sse240021008@iiti.ac.in>
2025-10-29 02:07:32 +0530

    Tables added

--- merchfrontend/src/app/home/page.jsx
+++ merchfrontend/src/app/home/page.jsx
@@ -1,15 +1,15 @@
 "use client";
 
-import Navbar from "../../components/Navbar";
 import Hero from "./components/Hero";
 import Categories from "./components/Categories";
 import FeaturedProducts from "./components/FeaturedProducts";
 import Footer from "../../components/Footer";
+import { NavbarFinal } from "@/components/Navbar";
 
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-white">
-      <Navbar />
+      <NavbarFinal />
       <Hero />
       <Categories />
       <FeaturedProducts />
