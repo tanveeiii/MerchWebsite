5d59cab3e91effc2693e3ec213a8104c92c0ccff
Kartikey <sse240021008@iiti.ac.in>
2025-11-02 19:05:07 +0530

    Add Address api added

--- merchbackend/src/user/user.controller.ts
+++ merchbackend/src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './userUpdate.dto';
+import { AddAddressDto } from './address.dto';
 
 @Controller('user')
 export class UserController {
@@ -9,5 +10,10 @@ export class UserController {
   @Put('update')
   update(@Body() dto: UpdateUserDto) {
     return this.userService.update(dto);
+  }
+
+  @Post('address')
+  addAddress(@Body() dto: AddAddressDto) {
+    return this.userService.addAddress(dto);
   }
 }
--- merchbackend/src/user/user.service.ts
+++ merchbackend/src/user/user.service.ts
@@ -1,13 +1,27 @@
-import { Body, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { UpdateUserDto } from './userUpdate.dto';
+import { AddAddressDto } from './address.dto';
 
 @Injectable({})
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async update(updateUserDTO: UpdateUserDto) {
+    const existingUser = await this.prisma.user.findFirst({
+      where: {
+        email: updateUserDTO.email,
+      },
+    });
+    if (!existingUser)
+      throw new NotFoundException({ code: 404, message: 'User not found' });
+
     const now = new Date();
     let updated_data = { updated_at: now };
     if (updateUserDTO.dob) updated_data['dob'] = updateUserDTO.dob;
@@ -31,6 +45,49 @@ export class UserService {
         created_at: true,
         updated_at: true,
         last_login: true,
+      },
+    });
+
+    return user;
+  }
+
+  async addAddress(addAddressDTO: AddAddressDto) {
+    const existingUser = await this.prisma.user.findFirst({
+      where: {
+        email: addAddressDTO.email,
+      },
+    });
+    if (!existingUser)
+      throw new NotFoundException({ code: 404, message: 'User not found' });
+    if (
+      !addAddressDTO.street_address ||
+      !addAddressDTO.postal_code ||
+      !addAddressDTO.state_province
+    )
+      throw new BadRequestException({ code: 400, message: 'Bad Request' });
+    const now = new Date();
+    const user = await this.prisma.address.create({
+      data: {
+        user_id: existingUser.user_id,
+        address_type: addAddressDTO.address_type,
+        street_address: addAddressDTO.street_address,
+        apartment_suite: addAddressDTO.apartment_suite,
+        city: addAddressDTO.city,
+        state_province: addAddressDTO.state_province,
+        postal_code: addAddressDTO.postal_code,
+        is_default: addAddressDTO.is_default,
+        created_at: now,
+        updated_at: now,
+      },
+      select: {
+        address_id: true,
+        user_id: true,
+        address_type: true,
+        street_address: true,
+        apartment_suite: true,
+        city: true,
+        state_province: true,
+        postal_code: true,
       },
     });
 
