1ad324542889c540dc1333213bd41e94867fd34a
YATHARTH-77 <yatharthm.2006@gmail.com>
2025-12-08 23:47:45 +0530

    corsenable

--- merchbackend/src/product/product.controller.ts
+++ merchbackend/src/product/product.controller.ts
@@ -1,13 +1,13 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { ProductService } from "./product.service";
-import { CreateProductDto } from "./product.dto";
+import { Body, Controller, Post } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDto } from './product.dto';
 
 @Controller('product')
-export class ProductController{
-    constructor(private  productService: ProductService) {}
-    @Post('create')
-    async create(@Body() dto:CreateProductDto){
-        const product = this.productService.create(dto);
-        return product;
-    }
+export class ProductController {
+  constructor(private productService: ProductService) {}
+  @Post('create')
+  async create(@Body() dto: CreateProductDto) {
+    const product = this.productService.create(dto);
+    return product;
+  }
 }
